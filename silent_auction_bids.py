2deb8b293c77f0a8c6e57f4b7eb8e345c8e9374c
Derek Redfern <redfern.derek@gmail.com>
2013-11-10 11:47:32 -0500

    Generates the packet as well as the sheets

--- silent_auction_bids.py
+++ silent_auction_bids.py
@@ -13,33 +13,46 @@ sample = open('FinalSilentAuction.csv', "rb")
 readSample = csv.reader(sample)
  
 f = open('servsheets.tex','w')
+g = open('servpacket.tex','w')
  
-print >>f, "\documentclass[11pt]{article}"
-print >>f, "\pagestyle{plain} \\topmargin -.5in \oddsidemargin 0in"
-print >>f, "\evensidemargin 0in \\textwidth 6.5in \\textheight 8in"
-print >>f, "\setlength{\parindent}{0in}"
+print >>f, "\\documentclass[11pt]{article}"
+print >>f, "\\pagestyle{plain} \\topmargin -.5in \oddsidemargin 0in"
+print >>f, "\\evensidemargin 0in \\textwidth 6.5in \\textheight 8in"
+print >>f, "\\setlength{\parindent}{0in}"
 print >>f, "\\begin{document}"
 
+print >>g, "\\documentclass[11pt]{article}"
+print >>g, "\\pagestyle{plain} \\topmargin -.5in \oddsidemargin 0in"
+print >>g, "\\evensidemargin 0in \\textwidth 6.5in \\textheight 8in"
+print >>g, "\\setlength{\parindent}{0in}"
+print >>g, "\\title{Silent Auction Donation Packet}"
+print >>g, "\\author{SERV Auction 2013}"
+print >>g, "\\date{November 11th, 2013 to November 15th, 2013}"
+print >>g, "\\begin{document}"
+print >>g, "\\maketitle"
+
 categories = [[],[],[],[],[],[],[]]
+categoryNames = ["Business Donations","Arts and Crafts","Events","Food","Lessons","Services","Miscellaneous"]
  
 for item in readSample:
-    if (item[5] == "Business Donations"):
+    if (item[5] == categoryNames[0]):
         categories[0].append(item)
-    elif (item[5] == "Arts and Crafts"):
+    elif (item[5] == categoryNames[1]):
         categories[1].append(item)
-    elif (item[5] == "Events"):
+    elif (item[5] == categoryNames[2]):
         categories[2].append(item)
-    elif (item[5] == "Food"):
+    elif (item[5] == categoryNames[3]):
         categories[3].append(item)
-    elif (item[5] == "Lessons"):
+    elif (item[5] == categoryNames[4]):
         categories[4].append(item)
-    elif (item[5] == "Services"):
+    elif (item[5] == categoryNames[5]):
         categories[5].append(item)
-    elif (item[5] == "Miscellaneous"):
+    elif (item[5] == categoryNames[6]):
         categories[6].append(item)
 
 for (i,category) in enumerate(categories): 
     category = sorted(category,key=lambda item: float(item[3].lstrip('$'))) # sort by minimum bid, lowest to highest
+    print >>g, "\section{"+categoryNames[i]+"}"
     for (j,item) in enumerate(category):
         print >>f, "\section*{"+str(i+1)+"."+str(j+1)+" "+handleLatexChars(item[2])+"}" # title
         print >>f, handleLatexChars(item[0]) # name
@@ -56,4 +69,12 @@ for (i,category) in enumerate(categories):
         print >>f, "\\end{tabular}"
         print >>f, "\\newpage"
 
+        print >>g, "\subsection{"+handleLatexChars(item[2])+"}" # title
+        print >>g, handleLatexChars(item[0]) # name
+        print >>g, "\\\\"
+        print >>g, "Starting Bid: "+handleLatexChars(addDollarSign(item[3]))
+        print >>g, "\\newline"
+        print >>g, handleLatexChars(item[4]).rstrip() # description
+
 print >>f, "\\end{document}"
+print >>g, "\\end{document}"
